fad8e8897795c42756d400f38054645ea69546ed
Jakob Grieshofer <jgrieshofer@student.tgm.ac.at>
2014-12-10 11:17:15 +0100

    Frame erstellt

---
+++ MyFrame.java
@@ -0,0 +1,25 @@
+package sindl_rasic_grieshofer_baum_koeck_a08;
+
+
+import javax.swing.JFrame;
+
+
+public class MyFrame extends JFrame {
+	/**
+	 * Dies ist der Konstruktor.
+	 * Hier wird das Fenster erstellt.
+	 * 
+	 * @param name Name des Fensters
+	 * @param panel Panel Objekt
+	 */
+	public MyFrame(MyPanel panel){
+		
+		this.setSize(700, 500);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.add(panel);
+		this.setVisible(true);
+		
+	}
+
+}
