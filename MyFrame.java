d471ed1449968fa204bc0c8de707194786686295
Mega Zerleger <dsindl@student.tgm.ac.at>
2014-12-12 18:06:44 +0100

    Update MyFrame.java

--- MyFrame.java
+++ MyFrame.java
@@ -1,25 +1,19 @@
 package sindl_rasic_grieshofer_baum_koeck_a08;
 
-
 import javax.swing.JFrame;
 
-
 public class MyFrame extends JFrame {
-	/**
-	 * Das ist der Konstruktor.
-	 * Hier wird das Fenster erstellt.
-	 * 
-	 * @param name Name des Fensters
-	 * @param panel Panel Objekt
-	 */
-	public MyFrame(MyPanel panel){
-		
-		this.setSize(500, 500);
-		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.add(panel);
-		this.setVisible(true);
-		
-	}
-
+  
+    /**
+     * Der Konstruktor
+     * @param panel 
+     */
+    public MyFrame(MyPanel panel){
+        this.setSize(500, 500);
+        this.setTitle("Lights out!");
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.add(panel);
+        this.setVisible(true);
+    }
 }
