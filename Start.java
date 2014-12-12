529a80a48e4de5a2e8965ca05a432df8d8f3544a
Mega Zerleger <dsindl@student.tgm.ac.at>
2014-12-12 18:07:06 +0100

    Update Start.java

--- Start.java
+++ Start.java
@@ -1,12 +1,15 @@
 package sindl_rasic_grieshofer_baum_koeck_a08;
 
-
 public class Start {
-	private static MyFrame myframe;
-	private static MyPanel mypanel;
-	public static void main(String[] args) {
-		mypanel = new MyPanel();
-		myframe = new MyFrame(mypanel);
-	}
-	
+    private static MyFrame myframe;
+    private static MyPanel mypanel;
+    
+    /**
+     * 
+     * @param args 
+     */
+    public static void main(String[] args) {
+        mypanel = new MyPanel();
+        myframe = new MyFrame(mypanel);
+    }
 }
