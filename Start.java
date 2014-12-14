8c7f752c91e089800a8bf7d2e07848aa8c33a453
Jakob Grieshofer <jgrieshofer@student.tgm.ac.at>
2014-12-14 16:18:04 +0100

    Update von Start

--- Start.java
+++ Start.java
@@ -1,15 +1,11 @@
 package sindl_rasic_grieshofer_baum_koeck_a08;
-
-public class Start {
-    private static MyFrame myframe;
-    private static MyPanel mypanel;
-    
-    /**
-     * 
-     * @param args 
-     */
-    public static void main(String[] args) {
-        mypanel = new MyPanel();
-        myframe = new MyFrame(mypanel);
-    }
+/**
+ * Start Klasse.
+ * @author Jakob Grieshofer
+ *
+ */
+public class Start {    
+	public static void main(String[] args) {
+		new MyControl();
+	}
 }
