08554f32b6b37efa5f933215caf92c55774ce82c
Mega Zerleger <dsindl@student.tgm.ac.at>
2014-12-15 09:13:45 +0100

    Update Start.java
    Nicht mehr Benötigt

--- Start.java
+++ Start.java
@@ -1,11 +1,12 @@
-package sindl_rasic_grieshofer_baum_koeck_a08;
-/**
+/**package sindl_rasic_grieshofer_baum_koeck_a08;
+
  * Start Klasse.
  * @author Jakob Grieshofer
  *
- */
-public class Start {    
-	public static void main(String[] args) {
-		new MyControl();
-	}
-}
+
+*public class Start {    
+*	public static void main(String[] args) {
+*		new MyControl();
+*	}
+*}
+*/ 
